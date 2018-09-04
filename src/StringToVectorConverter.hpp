d5b777609fe2f91731882b7d94038f96d87aec14
Adam Oleksy <adamoleksy1983@gmail.com>
2018-09-04 22:45:52 +0200

    Plik nagłówkowy z funkcją ConvertStringToVector która konwertuje string np.:3-|X|4/|5 i zwraca odpowieni vector: {3,0,10,4,6,5}

---
+++ src/StringToVectorConverter.hpp
@@ -0,0 +1,36 @@
+#ifndef STRINGTOVECTORCONVERTER_H
+#define STRINGTOVECTORCONVERTER_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> ConvertStringToVector(std::string str){
+
+    str.erase(std::remove(str.begin(), str.end(), '|'), str.end());
+    for(int i = 0; i < str.length(); i ++)
+        if(str[i] == '/')
+            str[i] = 10 - (str[i-1]-48);
+
+    std::vector<int> vec;
+
+    std::transform(str.begin(),
+                   str.end(),
+                   std::back_inserter(vec),
+                   [vec](char c) {
+                    if(c == '-'){
+                        return 0;
+                    }else if(c >= 48 && c<= 57 ){
+                        return c - 48;
+                    }else if(c == 'X') {
+                        return 10;
+                    }else if(c == '/'){
+
+                    }
+
+                     });
+
+    return vec;
+}
+
+#endif // STRINGTOVECTORCONVERTER_H
