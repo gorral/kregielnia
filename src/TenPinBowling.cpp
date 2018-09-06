138d8549dd81843a6dbcb05046a676b79da14f35
Andrzej Barecki <abarecki2@interia.pl>
2018-09-07 00:12:54 +0200

    update load inputFile

--- src/TenPinBowling.cpp
+++ src/TenPinBowling.cpp
@@ -8,28 +8,23 @@ TenPinBowling::TenPinBowling(std::string const & name)
 TenPinBowling::~TenPinBowling()
 {}
 
-std::string readFile(fs::directory_iterator & iter)
+std::string readFile(std::string & path)
 {
-    std::stringstream stream_path;
-
-    stream_path << iter->path().c_str();
-
-    std::ifstream ifsstream(stream_path.str());
+    std::ifstream ifsstream(path);
     std::string fileContent( (std::istreambuf_iterator<char>(ifsstream) ),
                                (std::istreambuf_iterator<char>()    ) );
-
     return fileContent;
 }
 
 bool TenPinBowling::loadInputFiles(std::string const &path)
 {
-    fs::directory_iterator iter(path), end;
+    //TODO: add error handling
+    std::string filePath;
+      for (auto & p : fs::directory_iterator(path)) {
+        filePath = p.path();
+        games_.push_back(std::make_tuple(readFile(filePath)));
+      }
 
-    for(;iter != end; iter++) {
-        //TODO:: cout only for debug
-        std::cout << readFile(iter);
-        games_.push_back(std::make_tuple(readFile(iter)));
-    }
     return true;
 }
 
