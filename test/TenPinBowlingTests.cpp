88e3c66d0b5865d2d5893bf1b54feb99c09f23cb
Adam Oleksy <adamoleksy1983@gmail.com>
2018-09-13 23:31:05 +0200

    Error fix

--- test/TenPinBowlingTests.cpp
+++ test/TenPinBowlingTests.cpp
@@ -19,26 +19,28 @@ TEST_F(TenPinBowlingTests, assertThatTravisCiBuildStatusIsPass)
     ASSERT_TRUE(true);
 }
 
-//TEST_F(TenPinBowlingTests, assertThatLoadInputFileFunctionReadAllFiles)
-//{
-    const std::string inputfiles_directory = "test_files";
-    fs::path testfile_path = fs::current_path() /= inputfiles_directory;
-//    int inFileCntr = 0;
+TEST_F(TenPinBowlingTests, assertThatLoadInputFileFunctionReadAllFiles)
+{
+    const std::string inputDirectoryPath = "test_files";
+    const fs::path outputFilePath = "result.txt";
+    int inFileCntr = 0;
 
-    TenPinBowling testgame{"My Bowling Game!", {}};
-//    // GIVEN
-        InputFile::Remove(inputfiles_directory);
-        ASSERT_FALSE(fs::is_directory(testfile_path));
-        ASSERT_TRUE(InputFile::Create(inputfiles_directory));
-        ASSERT_TRUE(fs::is_directory(testfile_path));
-        ASSERT_FALSE(fs::is_empty(testfile_path));
+    TenPinBowling testgame{"My Bowling Game!", {}, inputDirectoryPath, outputFilePath};
+    // GIVEN
+    InputFile::Remove(inputDirectoryPath);
+    ASSERT_FALSE(fs::is_directory(inputDirectoryPath));
+    ASSERT_TRUE(InputFile::Create(inputDirectoryPath));
+    ASSERT_TRUE(fs::is_directory(inputDirectoryPath));
+    ASSERT_FALSE(fs::is_empty(inputDirectoryPath));
 
-        for(auto&p: fs::directory_iterator(inputfiles_directory))
-            inFileCntr++;
-//    ASSERT_TRUE(testgame.loadInputFiles(testfile_path));
-    ASSERT_TRUE(testgame.loadInputFiles(testfile_path));
-//    ASSERT_TRUE(inFileCntr == testgame.gamesCntr());
-//}
+    for(auto&p: fs::directory_iterator(inputDirectoryPath)) {
+        inFileCntr++;
+    }
+    // WHEN
+    ASSERT_TRUE(testgame.loadInputFiles());
+    // THEN
+    ASSERT_TRUE(inFileCntr == testgame.gamesCntr());
+}
 
 TEST_F(TenPinBowlingTests, assertSingleNumberConversion)
 {
@@ -80,20 +82,21 @@ TEST_F(TenPinBowlingTests, assertCorrectPointsExtractionFromGameDescription)
 
 TEST_F(TenPinBowlingTests, assertCorrectGameStatusForOneLine)
 {
-    EXPECT_EQ(CheckStatusForPlayer(""), 0);
-    EXPECT_EQ(CheckStatusForPlayer(":"), 1);
-    EXPECT_EQ(CheckStatusForPlayer(":1"), 1);
-    EXPECT_EQ(CheckStatusForPlayer(":X|22|33"), 1);
-    EXPECT_EQ(CheckStatusForPlayer("Name1:X|4-|3"), 1);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|00"), 2);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|0"), 1);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55"), 1);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|54"), 2);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|X"), 1);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55||"), 1);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55||5"), 2);
-    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55||55"), 2);
+    EXPECT_EQ(CheckStatusForPlayer(""), Status::NotStarted);
+    EXPECT_EQ(CheckStatusForPlayer(":"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer(":1"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer(":X|22|33"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer("Name1:X|4-|3"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|00"), Status::Finished);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|0"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|54"), Status::Finished);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|X"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55||"), Status::InProgress);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55||5"), Status::Finished);
+    EXPECT_EQ(CheckStatusForPlayer("Name2:00|00|00|00|00|00|00|00|00|55||55"), Status::Finished);
 }
+
 TEST_F(TenPinBowlingTests, assertCorrectNameExtractionIfContainsColon)
 {
     EXPECT_EQ(GetPlayerName("Name:1:X|4-|3"), "Name:1");
